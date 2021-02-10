f0d16401648221357f985aae1ba598de0b21ad4e
Izz Hadri Hamidi <hadriizz.work@gmail.com>
2021-02-10 17:06:21 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ChatBody from './components/chatBody/ChatBody';
+import Nav from "./components/nav/Nav";
+
+function App() {
+  return (
+    <div className="__main">
+      <Nav />
+      <ChatBody />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/chatContent/ChatContent.js
@@ -0,0 +1,134 @@
+import React, { Component, useState, createRef, useEffect } from "react";
+
+import "./chatContent.css";
+import Avatar from "../chatList/Avatar";
+import ChatItem from "./ChatItem";
+
+export default class ChatContent extends Component {
+  messagesEndRef = createRef(null);
+  chatItms = [
+    {
+      key: 1,
+      image:
+        "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
+      type: "",
+      msg: "Hi Adib, apa khabar?",
+    },
+    {
+      key: 2,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+      type: "other",
+      msg: "Hi Izz, khabar baik.",
+    },
+    {
+      key: 3,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+      type: "other",
+      msg: "Awak mcm mana, sihat?",
+    },
+    {
+      key: 4,
+      image:
+        "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
+      type: "",
+      msg: "Alhamdulillah, sihat sejahtera.",
+    },
+    {
+      key: 5,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+      type: "other",
+      msg: "Free tak Ahad depan? Jom ajak kawan lain pergi main badminton.",
+    },
+    {
+      key: 6,
+      image:
+        "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
+      type: "",
+      msg: "Eh sekarang kan tengah lockdown. Mana boleh main berkumpul main badminton.",
+    },
+    {
+      key: 7,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+      type: "other",
+      msg: "Oh ye la kan, maaf.",
+    },
+  ];
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      chat: this.chatItms,
+      msg: "",
+    };
+  }
+
+  scrollToBottom = () => {
+    this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  onStateChange = (e) => {
+    this.setState({ msg: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="main__chatcontent">
+        <div className="content__header">
+          <div className="blocks">
+            <div className="current-chatting-user">
+              <Avatar
+                isOnline="active"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU"
+              />
+              <p>Muhd Adib</p>
+            </div>
+          </div>
+
+          <div className="blocks">
+            <div className="settings">
+              <button className="btn-nobg">
+                <i className="fa fa-cog"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="content__body">
+          <div className="chat__items">
+            {this.state.chat.map((itm, index) => {
+              return (
+                <ChatItem
+                  animationDelay={index + 2}
+                  key={itm.key}
+                  user={itm.type ? itm.type : "me"}
+                  msg={itm.msg}
+                  image={itm.image}
+                />
+              );
+            })}
+            <div ref={this.messagesEndRef} />
+          </div>
+        </div>
+        <div className="content__footer">
+          <div className="sendNewMessage">
+            <button className="addFiles">
+              <i className="fa fa-plus"></i>
+            </button>
+            <input
+              type="text"
+              placeholder="Type a message here"
+              onChange={this.onStateChange}
+              value={this.state.msg}
+            />
+            <button className="btnSendMsg" id="sendMsgBtn" onClick={this.sendMessage}>
+              <i className="fa fa-paper-plane"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
